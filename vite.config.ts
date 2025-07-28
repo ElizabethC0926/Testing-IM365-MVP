bbc22a96cde8063be7e6ecbae35ec63279f53f76
ElizabethC0926 <ewatsonew51@gmail.com>
2025-07-28 19:48:59 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Testing-IM365-MVP-main/' : './',
+  base: mode === 'production' ? '/Testing-IM365-MVP/' : './',
   server: {
     host: "::",
     port: 8080,
